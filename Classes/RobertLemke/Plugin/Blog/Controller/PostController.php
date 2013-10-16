52cdb6808550bd8c3f2687237a77cc789695f31c
Dominique Feyer <dfeyer@ttree.ch>
2013-10-16 17:17:52 +0200

    [TASK] Remove the TeaserPlugin, has we can now use TypoScript/EEL

--- Classes/RobertLemke/Plugin/Blog/Controller/PostController.php
+++ Classes/RobertLemke/Plugin/Blog/Controller/PostController.php
@@ -74,19 +74,6 @@ class PostController extends ActionController {
 	}
 
 	/**
-	 * Displays a list of most recent published posts
-	 */
-	public function teaserAction() {
-		/** @var Node $currentDocumentNode */
-		if ($this->request->getInternalArgument('__postsNodePath') !== NULL) {
-			$currentDocumentNode = $this->request->getInternalArgument('__documentNode');
-			$this->view->assign('posts', $currentDocumentNode->getNode($this->request->getInternalArgument('__postsNodePath'))->getChildNodes('RobertLemke.Plugin.Blog:Post', $this->request->getInternalArgument('__limit') ?: 2));
-		} else {
-			return 'Error: Please configure the postsNodePath settings';
-		}
-	}
-
-	/**
 	 * Renders an RSS feed
 	 *
 	 * @return string
